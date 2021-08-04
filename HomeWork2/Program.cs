690cf505d1c4308468f2e2a563eae35f30ae5224
soul_eraser@mail.ru <soul_eraser@mail.ru>
2021-08-04 13:17:17 +0300

    Добавьте файлы проекта.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork2 ///Студент Вечёрко Р.Д. 1) Написать метод, возвращающий минимальное из трёх чисел.
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+
+            int a,b,c,min;
+            Console.Write("Введите число a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число c:");  
+            c = Convert.ToInt32(Console.ReadLine());
+            min = Math.Min(Math.Min(a, b), Math.Min(b, c));
+            
+
+            Console.WriteLine("Наименьшее из чисел:" + min);
+            Console.ReadLine();
+        }
+    }
+}
