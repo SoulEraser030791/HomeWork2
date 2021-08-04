f1a20ea22b5cbc97827aa880aa6c59050be983d7
soul_eraser@mail.ru <soul_eraser@mail.ru>
2021-08-04 14:33:07 +0300

    HW2

---
+++ Ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex2 //// Студент Вечёрко Р.Д. 1) Написать метод подсчета количества цифр числа.
+{
+    class Program
+    {
+        private static int count;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое положительное число a:");
+            int a = int.Parse(Console.ReadLine());
+
+            while (a != 0)
+            {
+                count++;
+
+                a = a / 10;
+                Console.WriteLine(count);
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
