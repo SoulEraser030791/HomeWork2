05196424c6b98cec006da10dcddce02f0c7120f7
soul_eraser@mail.ru <soul_eraser@mail.ru>
2021-08-06 16:48:25 +0300

    1

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -7,7 +7,31 @@ namespace Ex3 //// Студент Вечёрко Р.Д. С клавиатуры 
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int A;
+            uint oddNumbers = 0;
+            do 
+        {
+            Console.WriteLine("Введите целое положительное число A");
+            A = int.Parse(Console.ReadLine());
+                int n = A % 2;
+                
+                if (n == 0)
+                {
+                    Console.WriteLine("Число является четным");
+                }
+                else 
+                {
+                    oddNumbers++;
+                    Console.WriteLine("Число является нечетным");
+                }
+                             
+                                
+        }
+            
+       while (A > 0) ;
+            Console.WriteLine("Количество введенных нечетных положительных чисел= " + oddNumbers);
+
+            
+        }
         }
     }
-}
